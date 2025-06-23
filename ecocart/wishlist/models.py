b7a83826d42a2faa60df7b191e5c39d2c583bc17
ridakhan2811 <rida10915@gmail.com>
2025-06-23 11:58:28 +0530

    23 june 2025

--- ecocart/wishlist/models.py
+++ ecocart/wishlist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class WishlistItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.product}"
--- ecocart/wishlist/views.py
+++ ecocart/wishlist/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import WishlistItem
+from products.models import Product
+from .forms import AddToWishlistForm
 
-# Create your views here.
+@login_required
+def wishlist_view(request):
+    wishlist = WishlistItem.objects.filter(user=request.user)
+    return render(request, 'wishlist/wishlist.html', {'wishlist': wishlist})
+
+@login_required
+def add_to_wishlist(request):
+    if request.method == 'POST':
+        form = AddToWishlistForm(request.POST)
+        if form.is_valid():
+            product = get_object_or_404(Product, id=form.cleaned_data['product_id'])
+            WishlistItem.objects.get_or_create(user=request.user, product=product)
+    return redirect('wishlist')
+
+@login_required
+def remove_from_wishlist(request, product_id):
+    WishlistItem.objects.filter(user=request.user, product__id=product_id).delete()
+    return redirect('wishlist')
