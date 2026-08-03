c0ab3d0c968b73d6afa4e60e9df489d7eb76cfd8
RutujaDeshmukh29 <rutujadeshmukh634@gmail.com>
2026-08-04 03:01:35 +0530

    Fix Vercel 500 crash by adding djangorestframework, trimming heavy packages, and fixing wsgi.py sys.path

--- ecocart/ecocart/wsgi.py
+++ ecocart/ecocart/wsgi.py
@@ -2,16 +2,23 @@
 WSGI config for ecocart project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Ensure ecocart project folder is in sys.path for Vercel serverless functions
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_dir = os.path.dirname(current_dir)
+root_dir = os.path.dirname(project_dir)
+
+for d in [project_dir, root_dir]:
+    if d not in sys.path:
+        sys.path.insert(0, d)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecocart.settings')
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
 app = application
